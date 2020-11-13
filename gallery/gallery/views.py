7d13b1a995aa31bd62446f57048ddb38964e0e3b
Rodgersouko <gabrielaouko@gmail.com>
2020-11-13 13:08:59 +0300

    create views.py for user interface

---
+++ gallery/gallery/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from gallery.models import gallarey
+
+def display(request):
+    resultsdisplay=gallarey.objects.all()
+    return render(request,'index.html',{'gallatey':resultsdisplay})
