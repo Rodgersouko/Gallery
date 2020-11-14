c11c9fc3651dc515c0c3beef61c7057ac02a191f
Rodgersouko <gabrielaouko@gmail.com>
2020-11-14 14:18:43 +0300

    add boostrapt styling

--- gallery/gallery/views.py
+++ gallery/gallery/views.py
@@ -3,4 +3,4 @@ from gallery.models import gallarey
 
 def display(request):
     resultsdisplay=gallarey.objects.all()
-    return render(request,'index.html',{'gallatey':resultsdisplay})
+    return render(request,'index.html',{'gallarey':resultsdisplay})
